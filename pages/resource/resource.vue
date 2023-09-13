bc24f97c293f0f91163ff309796fe371aab26531
AB-programming <WFB15376253289@outlook.com>
2023-09-13 18:11:42 +0800

    Add: ...

--- pages/resource/resource.vue
+++ pages/resource/resource.vue
@@ -1,13 +1,17 @@
 <template>
-	<view>
+	<u-tabs :list="list" @click="click"></u-tabs>
+	<view v-for="item in 3">
 		<u-skeleton :loading="false" rows="2" avatar title>
 			<view>
 				<view class="u-skeleton-slot">
 					<image src="/static/test.jpg" class="u-skeleton-slot__image"></image>
 					<view class="u-skeleton-slot__content">
 						<u--text text="利剑出鞘,一统江湖" type="main" size="14"></u--text>
-						<u--text type="tips" size="14" customStyle="margin-top: 5px"
-							text="众多组件覆盖开发过程的各个需求，组件功能丰富，多端兼容。让您快速集成，开箱即用"></u--text>
+						<view class="file" style="background: linear-gradient(#CCFBFF, #EF96C5);">
+							<u-text type="" size="20" text="学术论文"></u-text>
+							<u-text type="" size="20" text="PDF"></u-text>
+						</view><br>
+						<u-button type="primary" shape="circle" style="width: 40%;" plain icon="download">下载</u-button>
 					</view>
 				</view>
 			</view>
@@ -16,6 +20,19 @@
 </template>
 
 <script setup lang="ts">
+	const list = [
+		{
+			name: '全部'
+		},
+		{
+			name: '本校'
+		}
+	]
+	
+	const click = (item: any) => {
+		console.log(item)
+	}
+	
 </script>
 
 <style scoped lang="scss">
@@ -35,5 +52,23 @@
 			flex: 1;
 			padding: 20px;
 		}
+	}
+	.file {
+		margin-top: 20rpx;
+		// background: linear-gradient(#CCFBFF, #EF96C5);
+		// background: linear-gradient(#EEBD89, #D13ABD);
+		// background: linear-gradient(#C973FF, #AEBAF8);
+		// background: linear-gradient(#0C7BB3, #F2BAE8);
+		box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.5);
+		width: 400rpx;
+		height: 100rpx;
+		border-radius: 20rpx;
+		display: flex;
+		align-items: center;
+		justify-content: flex-end;
+	}
+	.file .extension {
+		
+		background-color: red;
 	}
 </style>
