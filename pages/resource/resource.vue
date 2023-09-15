eb12eaa6a69c6cb0d594e4d7dfd9385a56715114
AB-programming <WFB15376253289@outlook.com>
2023-09-15 16:45:56 +0800

    []

--- pages/resource/resource.vue
+++ pages/resource/resource.vue
@@ -1,25 +1,28 @@
 <template>
-	<u-tabs :list="list" @click="click"></u-tabs>
-	<view v-for="item in 3">
-		<u-skeleton :loading="false" rows="2" avatar title>
-			<view>
-				<view class="u-skeleton-slot">
-					<image src="/static/test.jpg" class="u-skeleton-slot__image"></image>
-					<view class="u-skeleton-slot__content">
-						<u--text text="利剑出鞘,一统江湖" type="main" size="14"></u--text>
-						<view class="file" style="background: linear-gradient(#CCFBFF, #EF96C5);">
-							<u-text type="" size="20" text="学术论文"></u-text>
-							<u-text type="" size="20" text="PDF"></u-text>
-						</view><br>
-						<u-button type="primary" shape="circle" style="width: 40%;" plain icon="download">下载</u-button>
-					</view>
-				</view>
-			</view>
-		</u-skeleton>
+	<view>
+		<u-tabs :list="list" @click="click"></u-tabs>
+		<view class="u-page">
+			<u-list @scrolltolower="scrolltolower">
+				<u-list-item v-for="(item, index) in 10" :key="index">
+					<u-cell :title="`列表长度-${index + 1}`" size="large" :border="false">
+						<template #icon>
+							<u-avatar shape="square" size="35" src="https://cdn.uviewui.com/uview/album/1.jpg"
+								customStyle="margin: -3px 5px -3px 0"></u-avatar>
+						</template>
+					</u-cell>
+					<u-text class="padding" :lines="2" text="关于uview-plus的取名来由，首字母u来自于uni-app首字母，
+					uni-app是基Vuejs，Vue和View(延伸为UI、视图之意)同音，同时view组件uni-app中
+					最础最重要的组件，故取名uview-plus，表达源于uni-app和Vue之意，同时在此也对它示感谢。"></u-text>	
+					<u-text class="padding" mode="link" text="Go to uview-plus docs" href="https://www.uviewui.com"></u-text>
+				</u-list-item>
+			</u-list>
+		</view>
 	</view>
 </template>
 
 <script setup lang="ts">
+	import { ref, reactive } from 'vue';
+
 	const list = [
 		{
 			name: '全部'
@@ -28,47 +31,41 @@
 			name: '本校'
 		}
 	]
-	
-	const click = (item: any) => {
+
+	const click = (item : any) => {
 		console.log(item)
 	}
-	
+
+	const indexList = reactive([]);
+	const urls = [
+		'https://cdn.uviewui.com/uview/album/1.jpg',
+		'https://cdn.uviewui.com/uview/album/2.jpg',
+		'https://cdn.uviewui.com/uview/album/3.jpg',
+		'https://cdn.uviewui.com/uview/album/4.jpg',
+		'https://cdn.uviewui.com/uview/album/5.jpg',
+		'https://cdn.uviewui.com/uview/album/6.jpg',
+		'https://cdn.uviewui.com/uview/album/7.jpg',
+		'https://cdn.uviewui.com/uview/album/8.jpg',
+		'https://cdn.uviewui.com/uview/album/9.jpg',
+		'https://cdn.uviewui.com/uview/album/10.jpg',
+	];
+
+	const onLoad = () => {
+		loadmore();
+	};
+
+	const scrolltolower = () => {
+		loadmore();
+	};
+
+	const loadmore = () => {
+		for (let i = 0; i < 30; i++) {
+			indexList.push({
+				url: 'https://cdn.uviewui.com/uview/album/1.jpg'
+			});
+		}
+	};
 </script>
 
 <style scoped lang="scss">
-	.u-skeleton-slot {
-		@include flex;
-		align-items: flex-start;
-
-		&__image {
-			width: 60px;
-			height: 60px;
-			border-radius: 100px;
-			margin: 10px 10px;
-		}
-
-		&__content {
-			margin-left: 20px;
-			flex: 1;
-			padding: 20px;
-		}
-	}
-	.file {
-		margin-top: 20rpx;
-		// background: linear-gradient(#CCFBFF, #EF96C5);
-		// background: linear-gradient(#EEBD89, #D13ABD);
-		// background: linear-gradient(#C973FF, #AEBAF8);
-		// background: linear-gradient(#0C7BB3, #F2BAE8);
-		box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.5);
-		width: 400rpx;
-		height: 100rpx;
-		border-radius: 20rpx;
-		display: flex;
-		align-items: center;
-		justify-content: flex-end;
-	}
-	.file .extension {
-		
-		background-color: red;
-	}
 </style>
